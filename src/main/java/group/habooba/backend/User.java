0b65c87dfa76de6586d02ffe65bd573abdf5611e
ALI <aloshpc20@gmail.com>
2025-11-19 14:38:56 +0800

    Added one attribute call PermissionList ,and constructors

--- src/main/java/group/habooba/backend/User.java
+++ src/main/java/group/habooba/backend/User.java
@@ -5,5 +5,17 @@ public class User {
     protected String password;
     protected String name;
     protected String email;
+    protected PermissionList permissionList;
+
+    User() {
+    }
+
+    User(long uid, String password) {
+        this.uid = uid;
+        this.password = password;
+    }
+
+
+
 
 }
