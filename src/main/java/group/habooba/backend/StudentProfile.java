d769d8dbbd6bff4c4202ea2d9c09515391ea8bbd
CJ Chui <darren.cww@gmail.com>
2025-11-20 15:08:23 +0800

    Create enum StudentProfile.java

---
+++ src/main/java/group/habooba/backend/StudentProfile.java
@@ -0,0 +1,15 @@
+package group.habooba.backend;
+
+public enum SchoolOfStudy {
+    NONE,
+    COMPUTER_SCIENCE,
+    LITERATURE,
+    MATHEMATICS,
+    PHILOSOPHY,
+    ENGINEERING,
+    PHYSICS,
+    ECONOMICS,
+    HISTORY,
+    BIOLOGY,
+    STATISTICS
+}
