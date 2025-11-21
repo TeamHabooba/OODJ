7dadb30de1a1959405e4eff7b6bf435ef6934cf6
w8lvi <alosh.hassn18@gmail.com>
2025-11-21 09:47:41 +0800

    The attribute was removed and the constructors were modified.

--- src/main/java/group/habooba/backend/ApplicationUncheckedException.java
+++ src/main/java/group/habooba/backend/ApplicationUncheckedException.java
@@ -1,15 +1,12 @@
 package group.habooba.backend;
 
 public class ApplicationUncheckedException extends RuntimeException {
-    protected String message;
 
     public ApplicationUncheckedException(String message) {
         super(message);
-        this.message = message;
     }
 
     public ApplicationUncheckedException(String message, Throwable cause) {
         super(message, cause);
-        this.message = message;
     }
 }
