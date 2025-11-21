0a00f8753275d9300a8dce24ca454265288c606b
w8lvi <alosh.hassn18@gmail.com>
2025-11-21 09:49:03 +0800

    The attribute was removed and the constructors were modified.

--- src/main/java/group/habooba/backend/ApplicationCheckedException.java
+++ src/main/java/group/habooba/backend/ApplicationCheckedException.java
@@ -1,15 +1,12 @@
 package group.habooba.backend;
 
 public class ApplicationCheckedException extends Exception {
-    protected String message;
 
     public ApplicationCheckedException(String message) {
         super(message);
-        this.message = message;
     }
 
     public ApplicationCheckedException(String message, Throwable cause) {
         super(message, cause);
-        this.message = message;
     }
 }
