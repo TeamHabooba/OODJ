0f05d7f7bd9d198294acb6954c4d2a2c5531b0e1
CJ Chui <darren.cww@gmail.com>
2025-11-21 10:22:47 +0800

    Create StudentProfile.java

--- src/main/java/group/habooba/backend/StudentProfile.java
+++ src/main/java/group/habooba/backend/StudentProfile.java
@@ -1,15 +1,52 @@
 package group.habooba.backend;
 
-public enum SchoolOfStudy {
-    NONE,
-    COMPUTER_SCIENCE,
-    LITERATURE,
-    MATHEMATICS,
-    PHILOSOPHY,
-    ENGINEERING,
-    PHYSICS,
-    ECONOMICS,
-    HISTORY,
-    BIOLOGY,
-    STATISTICS
+import java.util.List;
+
+public class StudentProfile {
+    private String firstName;
+    private String lastName;
+    private List<Course> courses;
+
+    public StudentProfile(String firstName, String lastName, List<Course> courses) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.courses = courses;
+    }
+
+
+    public String firstName() {
+        return firstName;
+    }
+
+    public String lastName() {
+        return lastName;
+    }
+
+    public String name() {
+        return firstName + " " + lastName;
+    }
+
+    public ProgramType program() {
+
+        return null;
+    }
+
+    public SchoolOfStudy schoolOfStudy() {
+
+        return SchoolOfStudy.NONE;
+    }
+
+    public int year() {
+
+        return 0;
+    }
+
+    public boolean nextLevelAllowed() {
+
+        return false;
+    }
+
+    public List<Course> courses() {
+        return courses;
+    }
 }
