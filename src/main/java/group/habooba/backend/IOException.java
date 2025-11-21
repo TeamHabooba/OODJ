dc45b9012dbf902dc3e84b1e2836dbf56969b634
w8lvi <alosh.hassn18@gmail.com>
2025-11-21 10:19:23 +0800

    Create IOException.java ,and added Constructors.

---
+++ src/main/java/group/habooba/backend/IOException.java
@@ -0,0 +1,12 @@
+package group.habooba.backend;
+
+public class IOException extends ApplicationCheckedException {
+
+    public IOException(String message) {
+        super(message);
+    }
+
+    public IOException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
