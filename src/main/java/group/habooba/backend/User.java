e46354c482d49ec3516d456f3e4925e29bc72263
w8lvi <alosh.hassn18@gmail.com>
2025-11-22 10:23:24 +0800

    Change the entire User.java ,added attributes,constructors,and Getters and Setters.

--- src/main/java/group/habooba/backend/User.java
+++ src/main/java/group/habooba/backend/User.java
@@ -1,21 +1,60 @@
 package group.habooba.backend;
 
 public class User {
+    // Protected - عشان Student يقدر يوصل لها
     protected long uid;
     protected String password;
-    protected String name;
     protected String email;
-    protected PermissionList permissionList;
 
+    // Public - حسب المخطط
+    public AttributeMap attributes;
+    public long policyId;
+
+    // Package-private constructors
     User() {
     }
 
-    User(long uid, String password) {
+    User(long uid, String password, String email) {
         this.uid = uid;
         this.password = password;
+        this.email = email;
     }
 
+    // Package-private methods
+    long uid() {
+        return uid;
+    }
 
+    void uid(long value) {
+        this.uid = value;
+    }
 
+    AttributeMap attributes() {
+        return attributes;
+    }
 
+    String email() {
+        return email;
+    }
+
+    void email(String value) {
+        this.email = value;
+    }
+
+    String password() {
+        return password;
+    }
+
+    void password(String value) {
+        this.password = value;
+    }
+
+    // Public methods
+    public PolicyAttributeMap attributes() {
+        return (PolicyAttributeMap) attributes;
+    }
+
+    public long policyId() {
+        return policyId;
+    }
 }
