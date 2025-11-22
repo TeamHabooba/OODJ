d684e5efc157727b42618dbd1599705f9a686bdd
w8lvi <alosh.hassn18@gmail.com>
2025-11-22 12:27:59 +0800

    Fix Course to be record with methods,and parameters.

--- src/main/java/group/habooba/backend/Course.java
+++ src/main/java/group/habooba/backend/Course.java
@@ -1,5 +1,26 @@
 package group.habooba.backend;
 
-record Course(ProgramType program, long id, String name) {
-}
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+public record Course(long id, String name, ProgramType program, List<Component> components) {
+
+    public Optional<Component> componentByName(String name) {
+        return components.stream()
+                .filter(component -> component.name().equals(name))
+                .findFirst();
+    }
+
+    public Optional<Component> componentById(long id) {
+        return components.stream()
+                .filter(component -> component.id() == id)
+                .findFirst();
+    }
+
+    public Map<String, Component> componentsMap() {
+        return components.stream()
+                .collect(Collectors.toMap(Component::name, component -> component));
+    }
+}
