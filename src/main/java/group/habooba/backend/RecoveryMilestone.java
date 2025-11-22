7e69f61a9d89478a42a63fa11b6f51f956752c00
w8lvi <alosh.hassn18@gmail.com>
2025-11-22 10:45:08 +0800

    Create RecoveryMilestone.java with only attributes.

---
+++ src/main/java/group/habooba/backend/RecoveryMilestone.java
@@ -0,0 +1,12 @@
+package group.habooba.backend;
+
+import java.util.ArrayList;
+
+public class RecoveryMilestone {
+
+    private Course course;
+    private ArrayList<ActionPlanEntry> actionPlan;
+    private float requiredGrade;
+    private float currentGrade;
+}
+
