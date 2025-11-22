dc36c1c54f1dd68aa6a4980f4bdcd44582b3b135
w8lvi <alosh.hassn18@gmail.com>
2025-11-22 12:14:03 +0800

    Add an comment

--- src/main/java/group/habooba/backend/RecoveryMilestone.java
+++ src/main/java/group/habooba/backend/RecoveryMilestone.java
@@ -6,6 +6,8 @@ public class RecoveryMilestone {
     private Enrollment enrollment;
     private ArrayList<ActionPlanEntry> actionPlan;
 
+// Not the final Change,it will modify in the future
+
     public boolean requiredGradeReached() {
         return false;
     }
